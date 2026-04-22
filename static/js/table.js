7b6b62998d7a3d88da382d353df6f6d86d4501be
105no7ob <121423590+T2head@users.noreply.github.com>
2026-04-22 18:00:03 +0800

    info security

--- static/js/table.js
+++ static/js/table.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     // 1. 初始化 DataTable
     if (!$.fn.DataTable.isDataTable("#resultTable")) {
         $("#resultTable").DataTable({ 
-            destroy: true
+            destroy: true,
             pageLength: 10, 
             lengthChange: false, 
             info: false 
